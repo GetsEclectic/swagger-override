e7a193ec3cf1c86d0c8905999a2656b48988824a
Ben Solwitz <ben.solwitz@nytimes.com>
2015-07-07 23:51:05 -0400

    confused why ASubClass doesn't appear in the json

--- src/test/java/GenericModel.java
+++ src/test/java/GenericModel.java
@@ -1,31 +1,47 @@
+import org.joda.time.DateTime;
+
+import javax.validation.constraints.AssertTrue;
+import javax.validation.constraints.Future;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 public class GenericModel {
-    private static final Map<String, Class<?>> additionalPropertyTypes = new HashMap<String, Class<?>>();
-    private Map<String, Object> additionalProperties;
+        public static final String INVALID_POSTAL_CODE = "no postal code or invalid postal code provided";
+        private static final Pattern US_ZIP_PATTERN = Pattern.compile("^\\d{5}(-\\d{4})?$");
+        public static final int CVV_MIN_LENGTH = 3;
+        public static final int CVV_MAX_LENGTH = 4;
+        private String name;
+        private String firstName;
+        private String lastName;
+        private String address1;
+        private String address2;
+        private String city;
+        private String state;
+        private String postalCode;
+        private String country;
+        private boolean useStratus = false;
+        private String number;
+        private String lastFourDigits;
+        private DateTime expirationDate;
+        private String groupId;
+        private String cvv;
+        private String cardAlias;
+        private Boolean defaultInstrument;
+        // this amount is not used for stratus, the caller should not be determining the amount
+        private String amount;
+        private BigDecimal updateCreditCardId;
+        private BigDecimal updatePaypalId;
+        // this is for testing purposes only
+        private Boolean forceStratusFailure = false;
+        private String currencyCode = "USD";
+        private ASubClass aSubClass;
 
-    public static void declareProperty(String key, Class<?> cls) {
-        additionalPropertyTypes.put(key, cls);
-    }
 
-    public static Map<String, Class<?>> getDeclaredProperties() {
-        return Collections.unmodifiableMap(additionalPropertyTypes);
-    }
-
-    public static void undeclareProperty(String key) {
-        additionalPropertyTypes.remove(key);
-    }
-
-    public void setProperty(String key, Object value) {
-        if (value == null || additionalPropertyTypes.get(key).isAssignableFrom(value.getClass())) {
-            additionalProperties.put(key, value);
+        public class ASubClass {
+                public int number;
         }
-    }
-
-    public Object getProperty(String key) {
-        return additionalProperties.get(key);
-    }
-
 }
